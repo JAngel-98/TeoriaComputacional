7bcef060edc5c63204f34451e9c2301aaa658b6f
Stelarino <55128190+Stelarino@users.noreply.github.com>
2019-09-13 12:11:49 -0500

    Update Juego.fs

--- Juego/Juego.fs
+++ Juego/Juego.fs
@@ -1,22 +1,23 @@
-﻿open System
+ open System
 
   type System.Random with
-  /// Generates an infinite sequence of random numbers within the given range.
+  //Genera numeros random y los guarda en una estructura de Secuencia.
   member this.GetValues(minValue, maxValue) =
       Seq.initInfinite (fun _ -> this.Next(minValue, maxValue))
-   
+   //Variable para el puntaje
  let mutable pts = 0
-  
+  //Función que imprime la matriz de 10x10
  let printGrid grid =
      
      
-
+     //Variables que definen la longitud de la matriz en filas y columnas
      let maxY = (Array2D.length1 grid) - 1
      let maxX = (Array2D.length2 grid) - 1
 
      printfn "    0 1 2 3 4 5 6 7 8 9"
      printfn "   ----------------------"
 
+     //Escribir en la consola los números correspondientes de la matriz
      for row in 0 .. maxY do
          printf " %d |" row 
          for col in 0 .. maxX do
@@ -28,14 +29,15 @@
              else Console.Write("0 ");
 
          Console.WriteLine()
- 
+     //Funcion donde se hace el borrado de números y se suben los ceros 
  let toggleGrid (grid : int[,]) =
      Console.WriteLine()
      Console.WriteLine("Escoge casilla:")
 
      let maxY = (Array2D.length1 grid) - 1
      let maxX = (Array2D.length2 grid) - 1
-     
+
+     //Se piden los valores de la posición de la casilla escogida
      let row =
          Console.Write("Fila: ")
          Console.ReadLine() |> int
@@ -45,11 +47,13 @@
          Console.ReadLine() |> int
       
 
+      //Valida y compara la posición de la casilla
      if (grid.[row, col] = grid.[row + 1, col]) && (grid.[row, col] = grid.[row - 1, col]) && (row <> 0) && (row <> 9) then 
         grid.[row , col] <- 0
         grid.[row + 1, col] <- 0
         grid.[row - 1, col] <- 0 
 
+        //Ciclo while para subir los ceros
         while (grid.[0,col] <> 0) do
 
          for i in 0 .. 8 do 
@@ -57,35 +61,38 @@
                let mutable aux = grid.[i,col]
                grid.[i,col] <- grid.[i+1,col]
                grid.[i+1,col] <- aux
-
+        //Si la casilla es correcta se obtiene el puntaje
         pts <- pts + 100
 
 
  let main() =
-
+    //Decalaracion de contadores y de filas/columnas
     let mutable cont = 0
 
     let rows = 10
     
     let cols = 10
-           
+    //Creación de la matriz 10x10 inicializados en 0       
     let grid = Array2D.zeroCreate<int> rows cols
 
+    //Se generan los números random 
     while cont < 10 do
         let mutable r = System.Random()
         let nums = r.GetValues(1, 6) |> Seq.take 10
        
-       
+       //Se crea un arreglo para 10 numeros random que se generan, generando 10 arreglos
         let nums1 =  Array.ofSeq nums
 
+        //Se le asigna los valores a la matriz
         for a in 0..9 do 
-            printfn "%d " nums1.[a]
+            printf "%d " nums1.[a]
             grid.[cont,a] <- nums1.[a]
 
         cont <- cont + 1
 
     printGrid grid
      
+    //Declaracion de los turnos que se tiene
     let mutable lives = 5
     while lives > 0 do
          printfn "Tienes %d turnos" lives
@@ -94,7 +101,7 @@
          printGrid grid
          lives <- lives - 1
          
-     
+    //Valida si el usuario ha acertado a todas las casillas 
     if pts = 500 then Console.WriteLine("FELICIDADES GANASTE")
     else Console.WriteLine("Gracias por jugar")
 
