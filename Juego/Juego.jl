3cb9337668d0cd0f9726f9e3de8a613b491a216d
JAngel-98 <angel-na@hotmail.es>
2019-09-13 01:09:08 -0500

    Julia Version Updated/Documented

--- Juego/Juego.jl
+++ Juego/Juego.jl
@@ -7,75 +7,74 @@ function input(msg) #Función de input, con mensaje personalizado
     readline() #Pide datos
 end
 
-function validaCoord(str)
-    if !(length(str) == 2) || !(isdigit(str[1])) || !(isletter(str[2]))
+function validaCoord(str) #Valida que el primer caracter sea numero y el segundo letra
+    if !(length(str) == 2) || !(isdigit(str[1])) || !(isletter(str[2])) || Int(str[2]) - 96 > 10
         return false
     end
     return true
 end
 
-function imprime(mtx)
+function imprime(mtx) #Imprime la matriz y el marco con las coordenadas
     clear()
-    println("    a b c d e f g h i j")
+    println("    a b c d e f g h i j") #Columnas
     println("  ,--------------------")
     for j in 1:size(mtx, 1)
-        print(j - 1, " | ")
+        print(j - 1, " | ") #Numero de fila
         for i in 1:size(mtx, 2)
-            print(mtx[j, i], " ")
+            print(mtx[j, i], " ") #Datos
         end
         println()
     end
     println("\n")
 end
 
-function xy(coor)
+function xy(coor) #Retorna los valores en decimal de las coordenadas
     x = Int(coor[2]) - 96
-    y = parse(Int, coor[1]) + 1
+    y = parse(Int, coor[1]) + 1 #Los arrays en Julia empiezan en 1
     return x, y
 end
 
-function buscaIguales(x, y, matrix, val)
+function buscaIguales(x, y, matrix, val) #Busca alrededor de una coordenada si son iguales
     if val == matrix[y, x]
-        push!(conj, parse(Int, string(y - 1, x - 1)))
-        matrix[y, x] = 0
-        if x > 1
+        push!(conj, parse(Int, string(y - 1, x - 1))) #Agrega las coordenadas pasadas a un array
+        matrix[y, x] = 0 #Asigna la casilla del array a 0, paro que no se cicle
+        if x > 1 #Busca en la derecha
             buscaIguales(x - 1, y, matrix, val)
         end
-        if y > 1
+        if y > 1 #Busca arriba
             buscaIguales(x, y - 1, matrix, val)
         end
-        if x < 10
+        if x < 10 #Busca en la izquierda
             buscaIguales(x + 1, y, matrix, val)
         end
-        if y < 10
+        if y < 10 #Busca abajo
             buscaIguales(x, y + 1, matrix, val)
         end
     end
 end
 
 function revisaLinea3(c1)
-    if length(c1) < 3
+    if length(c1) < 3 #Si el numero de iguales juntos es menor a 3, no es posible
         return false
     end
-    for i in c1
-        if in(i + 1, c1) && in(i + 2, c1)
+    for i in c1 #El el array se buscan las coincidencias
+        if in(i + 1, c1) && in(i + 2, c1) #Tres numeros consecutivos en x
             return true
         end
-        if in(i + 10, c1) && in(i + 20, c1)
+        if in(i + 10, c1) && in(i + 20, c1) #Tres numeros con diferencias de 10 en y
             return true
         end
     end
     return false
 end
 
-function subeCeros(mtx)
-    println("Sube")
-    for j in size(mtx, 1):-1:1
-        for i in 1:size(mtx, 2)
-            if (mtx[j, i] == 0) && j > 1
-                for n in j-1:-1:1
+function subeCeros(mtx) #Los ceros suben
+    for j in size(mtx, 1):-1:1 #Recorre toda la matriz desde abajo
+        for i in 1:size(mtx, 2) #Busca en todas las columnas
+            if (mtx[j, i] == 0) && j > 1 #Si es 0 y no esta en la cima...
+                for n in j-1:-1:1 #Busca el primer numero distinto de 0 arriba
                     if !(mtx[n, i] == 0)
-                        mtx[j, i], mtx[n, i] = mtx[n, i], 0
+                        mtx[j, i], mtx[n, i] = mtx[n, i], 0 #Cambia el numero por el 0
                         break
                     end
                 end
@@ -84,8 +83,8 @@ function subeCeros(mtx)
     end
 end
 
-function restauraMatrix(mtx, v)
-    for i in conj
+function restauraMatrix(mtx, v) #Regresa el valor que tenia antes de ponerle 0
+    for i in conj #Delas coordenadas en que habis buscado y guardado...
         i = string(i)
         if length(i) == 1
             mtx[1, parse(Int, i) + 1] = v
@@ -96,38 +95,40 @@ function restauraMatrix(mtx, v)
 
 end
 
-score = 0
-conj = Int[]
+score = 0 #Puntaje
+conj = Int[] #Conjunto de coordenadas guardadas
 
 function main()
-    matrix = rand(1:5, 10, 10)
+    matrix = rand(1:5, 10, 10) #Crea una matriz con numeros aleatorios del 1 al 5
     coor = ""
 
-    lifes = 5
+    lifes = 5 #Numero de vidas
     while lifes > 0
-        while true
-            imprime(matrix)
+        imprime(matrix)
+        while true #Pide las coordenadas, y valida que sea valida
             coor = input("Ingresa coordenadas (0a): ")
             if validaCoord(coor)
                 break
+            else
+                print(" * ")
             end
         end
 
-        global conj = Int[]
-        x, y = xy(coor)
-        val = matrix[y, x]
-        buscaIguales(x, y, matrix, matrix[y, x])
+        global conj = Int[] #Reinicia el array de coordenadas
+        x, y = xy(coor) #Coordenadas numericas
+        val = matrix[y, x] #Guarda el valor que esta en la coordenada, en caso de falso
+        buscaIguales(x, y, matrix, matrix[y, x]) #Busca iguales alrededor
         imprime(matrix)
-        if revisaLinea3(conj)
+        if revisaLinea3(conj) #Si hubo coincidencia de tres o mas, sube los cerosy aumenta puntaje
             subeCeros(matrix)
             global score += 100 * length(conj)
-        else
+        else #Si no, restaura la matriz al valor anterior
             restauraMatrix(matrix, val)
         end
-        imprime(matrix)
-        lifes -= 1
+        imprime(matrix) #Imprime matriz
+        lifes -= 1 #Resta una vida, haya sido cierto o falso el intento
     end
-    println("Puntaje Final: ", score)
+    println("Puntaje Final: ", score) #Muestra el puntaje final
 end
 
 main()
